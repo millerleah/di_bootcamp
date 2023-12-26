9d430037d3c901dd9385a5570a19dab3d99df9d8
millerleah <122454443+millerleah@users.noreply.github.com>
2023-12-26 21:06:39 +0200

    Add files via upload

---
+++ daily_challenge_dictionaries.py
@@ -0,0 +1,90 @@
+# Instructions
+# Challenge 1
+# Ask a user for a word
+# Write a program that creates a dictionary. This dictionary stores the indexes of each letter in a list.
+
+# Make sure the letters are the keys.
+# Make sure the letters are strings.
+# Make sure the indexes are stored in a list and those lists are values.
+# Examples
+
+# "dodo" ➞ { "d": [0, 2], "o": [1, 3] }
+
+# "froggy" ➞ { "f":  [0], "r": [1], "o": [2], "g": [3, 4], "y": [5] }
+
+# "grapes" ➞ { "g": [0], "r": [1], "a": [2], "p": [3]}
+
+# letter_index = input("Enter a word:")
+
+# my_dict = {}
+
+# for key, value in enumerate(letter_index):
+#    index = my_dict.setdefault(str(value), [])
+#    index.append(key)
+
+# print(my_dict)
+
+
+# Challenge 2
+# Create a program that prints a list of the items you can afford in the store with the money you have in your wallet.
+# Sort the list in alphabetical order.
+# Return “Nothing” if you can’t afford anything from the store.
+# Hint : make sure to convert the amount from dollars to numbers. Create a program that deletes the $ sign, and the comma (for thousands)
+
+items = {"Water": 1, "Bread": 3, "TV": 1000, "Fertilizer": 20}
+
+afford_list = []
+
+wallet = int(input('Amount of money in your wallet:').replace('$', '').replace(',', ''))
+
+for key, value in items.items():
+    # print(key, )
+    if wallet >= value:
+        afford_list.append(key)
+        sorted_list = sorted(afford_list)
+
+if len(afford_list) > 0:
+    print(f'These are the items you can afford:\n{', '.join(sorted_list)}')
+else:
+    print("Nothing")
+
+
+# Examples
+
+# The key is the product, the value is the price
+
+# items_purchase = {
+#   "Water": "$1",
+#   "Bread": "$3",
+#   "TV": "$1,000",
+#   "Fertilizer": "$20"
+# }
+
+# wallet = "$300"
+
+# ➞ ["Bread", "Fertilizer", "Water"]
+
+# items_purchase = {
+#   "Apple": "$4",
+#   "Honey": "$3",
+#   "Fan": "$14",
+#   "Bananas": "$4",
+#   "Pan": "$100",
+#   "Spoon": "$2"
+# }
+
+# wallet = "$100" 
+
+# ➞ ["Apple", "Bananas", "Fan", "Honey", "Spoon"]
+
+# items_purchase = {
+#   "Phone": "$999",
+#   "Speakers": "$300",
+#   "Laptop": "$5,000",
+#   "PC": "$1200"
+# }
+
+# wallet = "$1" 
+
+# ➞ "Nothing"
+    
