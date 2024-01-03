e2a77ef5d7a4be2730c944375666b38f3b9e1a93
millerleah <122454443+millerleah@users.noreply.github.com>
2024-01-03 16:55:07 +0200

    Update daily_challenge_tell_the_story.js

--- daily_challenge_tell_the_story.js
+++ daily_challenge_tell_the_story.js
@@ -72,7 +72,7 @@ function appendStory(story) {
   const text = document.getElementById("story");
   const span = document.createElement("span");
   span.innerText = story;
-  text?.appendChild(span);
+  text.appendChild(span);
 }
 
 function writeStory(noun, adjective, person, verb, place) {
@@ -80,7 +80,7 @@ function writeStory(noun, adjective, person, verb, place) {
 }
 
 const button = getButton();
-button?.addEventListener("click", click);
+button.addEventListener("click", click);
 
 // 5. Bonus: Add a “shuffle” button to the HTML file, when clicked the button should change the story currently displayed (but keep the values entered by the user). The user could click the button at least three times and get a new story. Display the stories randomly.
 
